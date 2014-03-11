bab16030b01e062c602b972d306661322d11157b
Nathan Beals <nathan_beals@hotmail.com>
2014-03-11 03:21:01 -0400

    Fixed compat.lua

--- lua/autorun/server/compat.lua
+++ lua/autorun/server/compat.lua
@@ -3,7 +3,10 @@
 duplicator.RegisterEntityModifier( "clips", function( p , ent , data)
 	if !IsValid(ent)  or !data then return end
 	
-	for _,clip in pairs(data) do
-		Clipping.NewClip( ent , {clip.n,clip.d})
-	end
+
+	timer.Simple(0.25 , function()
+		for _, clip in pairs(data) do
+			Clipping.NewClip( ent , {clip.n,clip.d})
+		end
+	end)
 end)
